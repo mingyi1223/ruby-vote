697095149e633f8fab805a63252c06fa964242eb
Josh Lin <mingyi1223@gmail.com>
2024-11-23 00:21:43 +0800

    create

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,9 +4,22 @@ class CandidatesController < ApplicationController
   end
 
   def new
+    @candidate = Candidate.new
   end
 
   def create
+
+    @candidate = Candidate.new(candidate_param)
+
+    if @candidate.save
+      flash[:notice] = "Candidate created!"
+      redirect_to '/candidates'
+    end
   end
-  
+
+  private
+  def candidate_param
+    params.require(:candidate).permit(:name, :party, :age, :politics)
+  end
+
 end
