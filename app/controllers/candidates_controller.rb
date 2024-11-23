611b15c56142b6b8b5aaf553222f6a6038e25239
Josh Lin <mingyi1223@gmail.com>
2024-11-24 01:23:45 +0800

    delete

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -40,6 +40,14 @@ class CandidatesController < ApplicationController
 
   end
 
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy
+
+    flash[:notice] = "Candidate deleted!"
+    redirect_to '/candidates'
+  end
+
   private
   def candidate_param
     params.require(:candidate).permit(:name, :party, :age, :politics)
